bb46ddecbf43d28c5430c35b79d549c966a066cd
jie <804957280@qq.com>
2024-10-26 13:33:48 +0800

    test

---
+++ Assets/Scripts/Gerneral/Character.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("基本属性")]
+    public float maxHealth;
+    public float currentHealth;
+    [Header("受伤无敌")]
+    public float invulnerableDuration;
+    private float invulnerableCounter;
+    public bool invulnerable;
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(Attack attacker)
+    {
+        if (invulnerable)
+            return;
+        //Debug.Log(attacker.damage);
+        if (currentHealth - attacker.damage > 0)
+        {
+            currentHealth -= attacker.damage;
+            TriggerInvulnerable();
+        }
+        else
+        {
+            currentHealth = 0;
+            //触发死亡
+        }
+    }
+    private void Update()
+    {
+        if (invulnerable)
+        {
+            invulnerableCounter -= Time.deltaTime;
+            if (invulnerableCounter <= 0)
+            {
+                invulnerable = false;
+            }
+        }
+    }
+    private void TriggerInvulnerable()
+    {
+        if (!invulnerable)
+        {
+            invulnerable = true;
+            invulnerableCounter = invulnerableDuration;
+        }
+    }
+}
